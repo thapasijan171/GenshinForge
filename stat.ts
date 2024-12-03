d2947330229d820f4345e3c11b5bd2df8fd6e093
thapasijan171 <thapasijan171@gmail.com>
2024-12-03 22:52:23 +0530

    feat(code): update initial code

---
+++ stat.ts
@@ -0,0 +1,8 @@
+class stat {
+    substat: string
+    value: number
+    constructor(type: string, stat: number) { // when initializing 3 stat artifacts, pass null into both args
+        this.substat = type
+        this.value = stat
+    }
+}
